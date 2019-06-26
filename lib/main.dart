3e9ee683fe92fcb9a7a6393614253e6e9ff574ec
elisangela.dborato <elisangela.borato@gmail.com>
2019-06-26 12:20:36 -0300

    main.dart e test...

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(color: Colors.grey,);
   }
 }
 
