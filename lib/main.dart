7a1b2f346033f44be39ff8098c362ca8ebf92907
Anderson-Bernardes <anderson_megax@hotmail.com>
2019-06-28 11:38:41 -0300

    criando telas principal cliente e empresa

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'view/tela_principal_cliente.dart';
+import 'view/tela_principal_empresa.dart';
 
 void main(){
   runApp(MaterialApp(
-    home: HomePage(),
+    home: TelaPrincipalEmpresa(),
   ));
 }
 
