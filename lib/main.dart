b1b05ef00bec7c4754de2350b9e36acd3a037447
leonardo.cberaldo1 <leocberaldo@hotmail.com>
2019-06-28 11:48:05 -0300

    Tentando fazer o merge
    Merge branch 'master' of https://github.com/elisangelaborato/pi into leonardo
    
    # Conflicts:
    #	lib/main.dart
    #	lib/view/tela_login.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-//<<<<<<< HEAD
-import 'package:pi/view/drawer.dart';
+//import 'package:pi/view/drawer.dart';
 import 'package:pi/view/tela_login.dart';
 
 void main(){
