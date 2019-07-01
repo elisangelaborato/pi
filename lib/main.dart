3256bdc4a92c97cc7428a9d830b0f7b686733843
Anderson-Bernardes <anderson_megax@hotmail.com>
2019-07-01 12:34:08 -0300

    Alterando tela principal do cliente e fazer tela principal empresa

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 //import 'package:pi/view/drawer.dart';
 import 'package:pi/view/tela_login.dart';
-import 'package:pi/view/tela_perfil_cliente.dart';
+import 'view/tela_principal_cliente.dart';
+import 'view/tela_principal_empresa.dart';
 
 void main(){
   runApp(MaterialApp(
-    home: TelaPerfilCliente(),
+    home: TelaPrincipalEmpresa(),
+
   ));
 }
 
